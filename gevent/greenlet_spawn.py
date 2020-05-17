b73ae984d0d1d09cd974410ad34453cb67944939
aditya singh <adityasingh27@hotmail.com>
2020-05-17 13:03:27 +0530

    Custom Greenlet Class

--- gevent/greenlet_spawn.py
+++ gevent/greenlet_spawn.py
@@ -25,3 +25,20 @@ threads = [thread1, thread2, thread3]
 
 # Block until all threads complete.
 gevent.joinall(threads)
+
+
+
+class MyGreenlet(Greenlet):
+
+    def __init__(self, message, n):
+        Greenlet.__init__(self)
+        self.message = message
+        self.n = n
+
+    def _run(self):
+        print(self.message)
+        gevent.sleep(self.n)
+
+g = MyGreenlet("Hi there!", 3)
+g.start()
+g.join()
